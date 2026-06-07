040eda6af9bd289c45b36c7b49a85b6ac0ef6c36
ts5750243 <59427962+ts5750243@users.noreply.github.com>
2026-06-06 21:32:56 -0600

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("🚀 Server running on port", PORT);
+});
+
 // Middleware
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
